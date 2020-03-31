0850935f891b3b1f60f3ba767e586df7ee67bd5f
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-30 23:57:21 -0700

    use case 3

--- reg.sh
+++ reg.sh
@@ -19,4 +19,6 @@ do
 		break
 	fi
 done
-[ "$userFlag" = "valid" ] && echo Valid || echo Invalid
+[ "$userFlag" = "valid" ] && echo Username is Valid || echo Username is Invalid
+read -p"Enter email id :" email
+[[ $email =~ ^[a-zA-Z0-9]+[\.\-\+\_]?[a-zA-Z0-9]*@[a-zA-Z]+[.]?[a-zA-Z]{2,4}[\.]?([a-z]{2})?$ ]] && echo Email is Valid || echo email is Invalid
